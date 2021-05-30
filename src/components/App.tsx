041c0214bba75c9861b40ead775a5819d0d9541c
Srud Salam <srud.salam@shareapp.co.uk>
2021-05-30 01:08:27 +0100

    initial setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,7 +7,7 @@ import "../styles.scss";
 import { Counter } from "./Counter";
 
 export default function App(): ReactElement {
-    const error = 5;
+    const error = 7;
     return (
         <>
             <h1>Simple React Boilerplate</h1>
